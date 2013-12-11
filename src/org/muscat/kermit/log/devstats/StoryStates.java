7374c889a0a5e0f9a134cd1b4bc16d90d0d88850
James Muscat <jrem@corefiling.com>
2013-12-11 14:17:15 +0000

    Compare story state sets.

--- src/org/muscat/kermit/log/devstats/StoryStates.java
+++ src/org/muscat/kermit/log/devstats/StoryStates.java
@@ -1,10 +1,12 @@
 package org.muscat.kermit.log.devstats;
 
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.jibble.pircbot.Colors;
 import org.muscat.kermit.log.LogEntry;
 
 public class StoryStates {
@@ -26,7 +28,70 @@ public class StoryStates {
   public Set<LogEntry> compareTo(final StoryStates newStates) {
     final Set<LogEntry> entries = new LinkedHashSet<LogEntry>();
 
+    for (final String story : _states.keySet()) {
+      if (newStates._states.containsKey(story)) {
+        final StoryState oldState = getState(story);
+        final StoryState newState = newStates.getState(story);
+        if (oldState != newState) {
+          entries.add(new StoryStateChangedEntry(oldState, newState, story));
+        }
+      }
+      else {
+        // story has been deleted
+        entries.add(new LogEntry() {
+          @Override public String getMessage() { return "Story " + Colors.YELLOW + story + Colors.NORMAL + " has been " + Colors.RED + "deleted" + Colors.NORMAL; }
+          @Override public Set<String> getChangedPaths() { return Collections.singleton(story);  }
+          @Override public String getChangeID() { return story; }
+        });
+      }
+
+      for (final String newStory : newStates._states.keySet()) {
+
+        if (!_states.containsKey(newStory)) {
+          // story has been added
+          entries.add(new LogEntry() {
+            @Override public String getMessage() { return "Story " + Colors.YELLOW + story + Colors.NORMAL + " has been " + Colors.GREEN + "created" + Colors.NORMAL + " (" + newStates.getState(story) + ")"; }
+            @Override public Set<String> getChangedPaths() { return Collections.singleton(story);  }
+            @Override public String getChangeID() { return story; }
+          });
+        }
+
+      }
+
+    }
+
     return entries;
   }
+
+
+  private final class StoryStateChangedEntry implements LogEntry {
+    private final StoryState _oldState;
+
+    private final StoryState _newState;
+
+    private final String _story;
+
+    private StoryStateChangedEntry(final StoryState oldState, final StoryState newState, final String story) {
+      _oldState = oldState;
+      _newState = newState;
+      _story = story;
+    }
+
+    @Override
+    public String getMessage() {
+      return "Story " + Colors.YELLOW + _story + Colors.NORMAL + " changed from " + _oldState.toString() + " to " + Colors.BOLD + _newState.toString() + Colors.NORMAL;
+    }
+
+    @Override
+    public Set<String> getChangedPaths() {
+      return Collections.singleton(_story);
+    }
+
+    @Override
+    public String getChangeID() {
+      return _story;
+    }
+  }
+
 
 }
